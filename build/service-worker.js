1de9cb1da0df95cd134ca3f8e51b528545d3fd71
Nhan Tran <tranvannhan100896@gmail.com>
2020-12-31 22:46:29 +0700

    Update content

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2d460c2021b228d32963c9ad8bcc93ba.js"
+  "/precache-manifest.ea5a8f58de8de865ee2cc1a641c12976.js"
 );
 
 self.addEventListener('message', (event) => {
